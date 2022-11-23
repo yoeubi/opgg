54e8a8c1c812ce2848850aacc691840cfe8b8ee2
성중원 <jungwon.seong@seongjung-won-ui-MacBookPro.local>
2022-11-24 03:23:06 +0900

    fix: add font on tierrank ui

--- src/components/TierRank.tsx
+++ src/components/TierRank.tsx
@@ -6,6 +6,15 @@ const TierRankContainer = styled.span`
   border-radius: 2px;
   border: solid 1px #d0d3d4;
   background-color: #e0e3e3;
+  font-family: Helvetica;
+  font-size: 11px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.42px;
+  color: #657070;
+  box-sizing: border-box;
   .season {
     font-weight: bold;
   }
