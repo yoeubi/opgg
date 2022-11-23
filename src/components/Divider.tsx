e3e5e886f1daa4c3e952e0400e997cd32e58f458
성중원 <jungwon.seong@seongjung-won-ui-MacBookPro.local>
2022-11-24 03:30:11 +0900

    feat: add divider ui

---
+++ src/components/Divider.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const DividerContainer = styled.div`
+  height: 1px;
+  background-color: #d8d8d8;
+`;
+
+const Divider = () => {
+  return <DividerContainer />;
+};
+
+export default Divider;
