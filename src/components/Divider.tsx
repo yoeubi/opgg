f31077f1feeb7366102e704783c3f56bef68ce60
성중원 <jungwon.seong@seongjung-won-ui-MacBookPro.local>
2022-12-04 04:31:18 +0900

    feat: add type in divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,12 +1,24 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const DividerContainer = styled.div`
-  height: 1px;
+const DividerContainer = styled.div<Props>`
+  ${(props) =>
+    props.type === "vertical"
+      ? css`
+          width: 1px;
+          height: 100%;
+        `
+      : css`
+          height: 1px;
+        `}
   background-color: #d8d8d8;
 `;
 
-const Divider = () => {
-  return <DividerContainer />;
+interface Props {
+  type?: "vertical" | "horizontal";
+}
+
+const Divider = ({ type = "horizontal" }: Props) => {
+  return <DividerContainer type={type} />;
 };
 
 export default Divider;
